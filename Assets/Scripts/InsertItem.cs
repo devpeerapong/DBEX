1b5e79215f2ded49b906947cd6f907c86df37b43
lactozily <lactoz.ily@gmail.com>
2016-04-26 14:52:54 +0700

    m

---
+++ Assets/Scripts/InsertItem.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.SqliteClient;
+using System.Data;
+using System;
+using UnityEngine.UI;
+
+public class InsertItem : MonoBehaviour {
+
+	public InputField nameInput;
+	public InputField passwordInput;
+	public InputField ageInput;
+	public Text Content;
+
+	public void Insert(){
+		string connectionString = "URI=file:" + Application.dataPath + "/myDatabase.db"; //Path to database.
+		IDbConnection dbconn;
+		dbconn = (IDbConnection) new SqliteConnection(connectionString);
+
+		dbconn.Open(); //Open connection to the database.
+
+		IDbCommand dbcmd = dbconn.CreateCommand();
+
+		string sqlQuery = "insert into Account (userName,password, age) values ('"+ nameInput.text +"','"+ passwordInput.text + "','" + ageInput.text + "')";
+		Debug.Log (sqlQuery);
+		dbcmd.CommandText = sqlQuery;
+
+		IDataReader reader = dbcmd.ExecuteReader();
+
+		reader.Close();reader = null;
+		dbcmd.Dispose();dbcmd = null;
+		dbconn.Close();dbconn = null;
+
+		nameInput = "";
+		passwordInput = "";
+		ageInput = ""
+
+		Read ();
+	}
+
+	public void Read() {
+		string connectionString = "URI=file:" + Application.dataPath + "/myDatabase.db"; //Path to database.
+		IDbConnection dbconn;
+		dbconn = (IDbConnection) new SqliteConnection(connectionString);
+
+		dbconn.Open(); //Open connection to the database.
+
+		IDbCommand dbcmd = dbconn.CreateCommand();
+
+		string sqlQuery = "select * from Account";
+		dbcmd.CommandText = sqlQuery;
+
+		IDataReader reader = dbcmd.ExecuteReader();
+
+		Content.text = "";
+		while (reader.Read ()) {
+			string username = reader.GetString (1);
+			string password = reader.GetString (2);
+			int age = reader.GetInt32(3);
+
+			Content.text += username + " - " + password + " - " + age.ToString () + "\n";
+		}
+
+		reader.Close();reader = null;
+		dbcmd.Dispose();dbcmd = null;
+		dbconn.Close();dbconn = null;
+	}
+		
+}
